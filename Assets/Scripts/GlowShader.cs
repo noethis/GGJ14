1b1469b5abe518865696ed0e5bed1ca23b156c2a
azoerb <azoerb1@gmail.com>
2014-01-25 19:17:01 -0600

    Adding in SoundRay

---
+++ Assets/Scripts/GlowShader.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlowShader : MonoBehaviour {
+	float effectTime;
+	
+	void Update () {
+		if (effectTime > 0) {
+			if (effectTime > 300){
+				renderer.sharedMaterial.SetVector("_ShieldColor", new Vector4(1, 1, 1, 0.5f));
+			}
+			
+			effectTime -= Time.deltaTime * 1000;
+			
+			renderer.sharedMaterial.SetFloat("_EffectTime", effectTime);
+		}
+	}
+	
+	void OnTriggerEnter(Collider col) {
+		
+		Vector3 contact = collider.ClosestPointOnBounds (col.gameObject.transform.localPosition);
+			
+		renderer.sharedMaterial.SetVector("_ShieldColor", new Vector4(1, 1, 1, 0.5f));
+			
+		renderer.sharedMaterial.SetVector("_Position", transform.InverseTransformPoint(contact));
+		print (contact);
+			
+		effectTime = 500;
+	}
+}
--- Assets/Scripts/PlayerSound.cs
+++ Assets/Scripts/PlayerSound.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 
 public class PlayerSound : PlayerController {
 
+	public SoundRay soundRay;
+
 	// Use this for initialization
 	protected virtual void Start () {
 		base.Start ();
@@ -17,5 +19,12 @@ public class PlayerSound : PlayerController {
 	// Update is called once per physics frame
 	protected virtual void FixedUpdate () {
 		base.FixedUpdate ();
+		checkInput ();
+	}
+
+	void checkInput() {
+		if (GameState.Instance.activePlayer && Input.GetMouseButton (0)) {
+
+		}
 	}
 }
