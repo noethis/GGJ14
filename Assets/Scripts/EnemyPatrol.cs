580241ed97126e201c2075363e138c3f37789c28
Erik <ewstadler@gmail.com>
2014-01-25 21:20:33 -0600

    Enemies can now see players

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -8,6 +8,14 @@ public class EnemyPatrol : MonoBehaviour {
 	public float waypointWaitTime;
 	private NavMeshAgent agent;
 	private EnemyWaypoint currTarget;
+	public bool playerInSight;
+	public float fieldOfViewAngle = 110f;
+	private SphereCollider col;
+
+	void Awake() 
+	{
+		col = GetComponent<SphereCollider>();
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -17,7 +25,7 @@ public class EnemyPatrol : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
 	public void SetNextWaypoint() {
@@ -57,5 +65,60 @@ public class EnemyPatrol : MonoBehaviour {
 				Invoke( "SetNextWaypoint", waypointWaitTime );
 			}
 		}
+
+		// If the player has entered the trigger sphere...
+		if (other.CompareTag("Player"))
+		{
+			// By default the player is not in sight.
+			playerInSight = false;
+
+			// Create a vector from the enemy to the player and store the angle between it and forward.
+			Vector3 direction = other.transform.position - transform.position;
+			float angle = Vector3.Angle(direction, transform.forward);
+			
+			// If the angle between forward and where the player is, is less than half the angle of view...
+			if(angle < fieldOfViewAngle * 0.5f)
+			{
+				RaycastHit hit;
+				// ... and if a raycast towards the player hits something...
+				//if(Physics.Raycast(transform.position + transform.up, direction.normalized, out hit, 30))
+				if(Physics.Raycast(transform.position, direction.normalized, out hit, 8))
+				{
+					Debug.DrawLine (other.transform.position, transform.position);
+					// ... and if the raycast hits the player...
+					if(hit.collider.gameObject.CompareTag("Player"))
+					{
+						// ... the player is in sight.
+						playerInSight = true;
+						print ("found player");
+					}
+				}
+			}
+		}
+	}
+
+	/*
+	bool CanSeePlayer() {
+		//if LOS to player
+		Vector3 dir = opponent.transform.position - myTransform.position;
+		Vector3 pos = myTransform.position;
+		//pos.y = SHOOT_POS_Y;
+		Ray ray = new Ray( pos, dir.normalized );
+		RaycastHit hit;
+		int layerMask = ~(1<<10); //see through clip
+		if ( Physics.Raycast( ray, out hit, Mathf.Infinity, layerMask ) && hit.collider != null && hit.collider.CompareTag( "Player" ) ) {
+			return true;
+		}
+		return false;
+	}
+	*/
+
+	void OnTriggerExit (Collider other)
+	{
+		// If the player leaves the trigger zone...
+		if(other.CompareTag("Player"))
+			// ... the player is not in sight.
+			playerInSight = false;
+			print ("lost player");
 	}
 }
