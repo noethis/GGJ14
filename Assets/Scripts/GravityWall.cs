a59d9b9115628fff36ac35b0a1531c6428f29184
Erik <ewstadler@gmail.com>
2014-01-25 22:25:37 -0600

    Added GravityWall and Level Walls

---
+++ Assets/Scripts/GravityWall.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityWall : MonoBehaviour {
+
+	public float force = 10;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerStay (Collider other)
+	{
+		other.rigidbody.AddForce (Vector3.forward * force, ForceMode.Impulse);
+	}
+}
