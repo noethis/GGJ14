1ddac599ed1f4385fde89649c2cf77a310ee7058
azoerb <azoerb1@gmail.com>
2014-01-26 13:57:33 -0600

    Update Enemy View Script

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -8,13 +8,13 @@ public class EnemyPatrol : MonoBehaviour {
 	public float waypointWaitTime;
 	private NavMeshAgent agent;
 	private EnemyWaypoint currTarget;
-	public bool playerInSight;
-	public float fieldOfViewAngle = 110f;
+	private bool playerInSight = false;
+	public float fieldOfViewAngle = 45f;
+	public float viewLength = 2;
 	private SphereCollider col;
 	public AudioClip footsteps;
 
-	void Awake() 
-	{
+	void Awake() {
 		col = GetComponent<SphereCollider>();
 	}
 
@@ -34,14 +34,24 @@ public class EnemyPatrol : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-
+		foreach (PlayerController player in GameState.Instance.players) {
+			Vector3 toPlayer = player.transform.position - transform.position;
+			if (toPlayer.magnitude >= viewLength) {
+				float angle = Vector3.Angle(toPlayer, transform.forward);
+				if (angle < fieldOfViewAngle * 0.5f) {
+					RaycastHit hit;
+					if(Physics.Raycast(transform.position, toPlayer.normalized, out hit, 8)) {
+						if(hit.collider.gameObject.CompareTag("Player")) {
+							playerInSight = true;
+						}
+					}
+				}
+			}
+		}
 	}
 
 	public void SetNextWaypoint() {
 		if (currTarget == null) {
-			if (targets == null || targets.Count == 0) {
-				currTarget = null;
-			}
 			currTarget = targets [Random.Range(0, targets.Count)];
 		} 
 		else {
@@ -74,60 +84,5 @@ public class EnemyPatrol : MonoBehaviour {
 				Invoke( "SetNextWaypoint", waypointWaitTime );
 			}
 		}
-
-		// If the player has entered the trigger sphere...
-		if (other.CompareTag("Player"))
-		{
-			// By default the player is not in sight.
-			playerInSight = false;
-
-			// Create a vector from the enemy to the player and store the angle between it and forward.
-			Vector3 direction = other.transform.position - transform.position;
-			float angle = Vector3.Angle(direction, transform.forward);
-			
-			// If the angle between forward and where the player is, is less than half the angle of view...
-			if (angle < fieldOfViewAngle * 0.5f)
-			{
-				RaycastHit hit;
-				// ... and if a raycast towards the player hits something...
-				//if(Physics.Raycast(transform.position + transform.up, direction.normalized, out hit, 30))
-				if(Physics.Raycast(transform.position, direction.normalized, out hit, 8))
-				{
-					Debug.DrawLine (other.transform.position, transform.position);
-					// ... and if the raycast hits the player...
-					if(hit.collider.gameObject.CompareTag("Player"))
-					{
-						// ... the player is in sight.
-						playerInSight = true;
-						print ("found player");
-					}
-				}
-			}
-		}
-	}
-
-	/*
-	bool CanSeePlayer() {
-		//if LOS to player
-		Vector3 dir = opponent.transform.position - myTransform.position;
-		Vector3 pos = myTransform.position;
-		//pos.y = SHOOT_POS_Y;
-		Ray ray = new Ray( pos, dir.normalized );
-		RaycastHit hit;
-		int layerMask = ~(1<<10); //see through clip
-		if ( Physics.Raycast( ray, out hit, Mathf.Infinity, layerMask ) && hit.collider != null && hit.collider.CompareTag( "Player" ) ) {
-			return true;
-		}
-		return false;
-	}
-	*/
-
-	void OnTriggerExit (Collider other)
-	{
-		// If the player leaves the trigger zone...
-		if(other.CompareTag("Player"))
-			// ... the player is not in sight.
-			playerInSight = false;
-			print ("lost player");
 	}
 }
